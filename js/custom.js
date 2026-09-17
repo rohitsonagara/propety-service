f8556d9ed4375299d62dbbc86825899ab00cbc97
rohitsonagara <rohitsonagar25@gmail.com>
2026-09-17 17:00:28 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+ document.addEventListener(
+                "DOMContentLoaded", () => {
+                    new Mmenu( "#menu", {
+                       "offCanvas": {
+                          "position": "left"
+                       },
+                       "theme": "light"
+                    });
+                }
+            );
+
+ var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.maltipal').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
